caf737d8206736b2df69a4d73a36b22c729c54ac
Margarita Gamarra <122489298+mrgamarra22@users.noreply.github.com>
2025-08-09 12:46:31 -0500

    Image_Segmentation.py

--- Image_Segmentation.py
+++ Image_Segmentation.py
@@ -119,15 +119,15 @@ plt.axis('off')
 gray_astronaut_noiseless = gaussian(gray_astronaut, 1)
 
 # Localising the circle's center at 220, 110
-x1 = 220 + 100*np.cos(np.linspace(0, 2*np.pi, 500))
-x2 = 100 + 100*np.sin(np.linspace(0, 2*np.pi, 500))
+x2 = 220 + 100*np.cos(np.linspace(0, 2*np.pi, 400))
+x1 = 100 + 100*np.sin(np.linspace(0, 2*np.pi, 400))
 
 # Generating a circle based on x1, x2
 snake = np.array([x1, x2]).T
 
 # Computing the Active Contour for the given image
 astronaut_snake = active_contour(gray_astronaut_noiseless,
-								snake)
+								snake, alpha=0.015, beta=10, gamma=0.001)
 
 fig = plt.figure(figsize=(10, 10))
 
@@ -138,12 +138,12 @@ ax = fig.add_subplot(111)
 ax.imshow(gray_astronaut_noiseless, cmap = 'gray')
 
 # Plotting the face boundary marker
-ax.plot(astronaut_snake[:, 0],
-		astronaut_snake[:, 1], 
+ax.plot(astronaut_snake[:, 1],
+		astronaut_snake[:, 0], 
 		'-b', lw=5)
 
 # Plotting the circle around face
-ax.plot(snake[:, 0], snake[:, 1], '--r', lw=5)
+ax.plot(snake[:, 1], snake[:, 0], '--r', lw=5)
 """
 
 
@@ -206,3 +206,4 @@ plt.subplot(1,2,2)
 plt.imshow(label2rgb(astronaut_segments,
 					astronaut,
 					kind = 'avg'))
+
